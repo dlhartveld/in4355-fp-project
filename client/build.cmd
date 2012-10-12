9454fa076fa9664ff7d8e231ad7ed9060aeb1659
Michael de Jong <michaelj@minicom.nl>
2012-10-12 10:20:01 +0200

    Trying to mavenize the project.

--- client/build.cmd
+++ client/build.cmd
@@ -1,5 +1,5 @@
 rmdir /s /q target
 mkdir target
 
-xcopy /v /l /s src\main\webapp\* .\target\
-fay < src\main\fay\script.hs > .\target\script.js
+xcopy /v /l /s ..\src\main\webapp\* .\target\
+fay < ..\src\main\fay\script.hs > .\target\script.js
--- client/build.sh
+++ client/build.sh
@@ -2,4 +2,4 @@ rm -rf target
 mkdir target
 
 cp -rv ../src/main/webapp/* ./target/
-fay < src/main/fay/script.hs > target/script.js
+fay < ../src/main/fay/script.hs > target/script.js
