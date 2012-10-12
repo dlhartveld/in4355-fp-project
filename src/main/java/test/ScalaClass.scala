ad3f1a54aa2cf8933771b69e48f45f8adc5675b3
Michael de Jong <michaelj@minicom.nl>
2012-10-12 11:45:16 +0200

    Attempting to add jersey to the project.

--- src/main/java/test/ScalaClass.scala
+++ src/main/java/test/ScalaClass.scala
@@ -1,5 +1,12 @@
 package test
 
-class ScalaClass {
+import javax.ws.rs._
 
+@Path("/hello")
+class ScalaClass {
+  
+  @GET @Produces(Array("text/html"))
+  def doGet = "<html><body><h1>hello jersey/scala world!</h1></body></html>";
+  
+    
 }
