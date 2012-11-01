04c15aaec16afd7b19cb7d6a0c2da99ad4b16115
Michael de Jong <michaelj@minicom.nl>
2012-11-01 12:44:50 +0100

    Attempt at word count reduce phase.

--- src/main/java/nl/tudelft/ewi/se/in4355/server/jobs/wordcount/WordCount.java
+++ src/main/java/nl/tudelft/ewi/se/in4355/server/jobs/wordcount/WordCount.java
@@ -1,15 +1,21 @@
 package nl.tudelft.ewi.se.in4355.server.jobs.wordcount;
 
-class WordCount{
+class WordCount implements Comparable<WordCount> {
 	public final String word;
 	public final int count;
-	
+
 	public WordCount() {
 		this("", -1);
 	}
-	
+
 	public WordCount(String word, int count) {
 		this.word = word;
 		this.count = count;
 	}
+
+	@Override
+	public int compareTo(WordCount other) {
+		return word.compareTo(other.word);
+	}
+
 }
---
+++ src/main/java/nl/tudelft/ewi/se/in4355/server/jobs/wordcount/WordIndex.scala
@@ -0,0 +1,40 @@
+package nl.tudelft.ewi.se.in4355.server.jobs.wordcount
+
+import scala.collection.JavaConversions
+
+class WordIndex {
+
+  val limit = 100;
+
+  private var words = List[WordCount]();
+
+  def insert(word: WordCount) {
+    this.synchronized {
+      words = words.+:(word).sortBy(_.word);
+    }
+  }
+
+  def take: List[WordCount] = {
+    this.synchronized {
+      var amount = limit;
+      if (words.size < limit) {
+        amount = words.size;
+      }
+      var result = words.take(amount);
+      words = words.drop(amount);
+      return result;
+    }
+  }
+
+  def takeAll: scala.collection.immutable.List[WordCount] = {
+    this.synchronized {
+      var amount = words.size;
+      var result = words.take(amount);
+      words = words.drop(amount);
+      return words;
+    }
+  }
+
+  def size = words.size;
+
+}
